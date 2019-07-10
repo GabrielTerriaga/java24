c10be9d7599ad45a174264fce71abc16c31d7aba
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-07-10 13:48:16 -0300

    Metodos Abstract

---
+++ src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Shape> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Shape #" + i + " data:");
+			System.out.print("Rectangle or Circle (r/c)? ");
+			char select = sc.next().charAt(0);
+			System.out.print("Color (BLACK/BLUE/RED): ");
+			Color color = Color.valueOf(sc.next());
+			
+			if (select == 'r') {			
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+				
+				list.add(new Rectangle(color, width, height));
+			}
+			else if (select == 'c') {
+				System.out.print("Radius: ");
+				double radius = sc.nextDouble();
+				
+				list.add(new Circle(color, radius));
+			}//end if
+		}//end for
+		
+		System.out.println();
+		System.out.println("SHAPE AREAS: ");
+		for (Shape s : list) {
+			System.out.println(String.format("%.2f", s.area()));
+		}
+		
+		sc.close();
+	}
+
+}
